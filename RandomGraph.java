8a64a449459ac329cba21a058f223869303983bb
bbaclig <bbaclig@ucsd.edu>
2014-02-27 21:19:46 -0800

    finished what ever i was doing last time except need to do for n=100, also need to output to some file

--- RandomGraph.java
+++ RandomGraph.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 /* 
  * Graph is represented by an ArrayList of Vertex objects, each Vertex object
@@ -92,32 +93,116 @@ public class RandomGraph {
     }
     */
 
-    // Test main for now..
+    // Calculate mean
+    private static double compute_mean(double a[], int n) {
+        if(n== 0)
+            return 0.0;
+        double sum= 0;
+        for(int i= 0; i< n; i++) {
+            sum+= a[i];
+        }
+        double mean= sum/ n;
+        return mean;
+    }
+
+    // Calculate standard deviation
+    private static double std_dev(double a[], int n, double mean) {
+        if(n == 0)
+            return 0.0;
+        double sq_diff_sum = 0;
+        for(int i = 0; i < n; ++i) {
+            double diff = a[i] - mean;
+            sq_diff_sum += diff * diff;
+        }
+        double variance = sq_diff_sum / n;
+        return Math.sqrt(variance);
+} 
+
+/********************************** MAIN ****************************************/
+
+    // Main
     public static void main( String [] args ) {
+        // I/O stuff
+        File outFile= new File("n20.txt");
+        PrintWriter out= new PrintWriter(new FileWriter(outFile));   
+
+        /* 
+        try {
+            out= new PrintWriter(new FileWriter(outFile));
+        } catch (Exception e) {
+            System.out.println("SOMETHING WRONG~");
+        }*/
+
         /***** For n = 20 *****/
         int n= 20;
         int k= 200;
+        // an array to store each k, varies for each p
+        double[] kCC= new double[200];
         // an array to store mean for n= 20
-        double[] meanTwenty= new double[50];
+        double[] meanTwenty= new double[51];
         // an array to store sd for n= 20
-        double[] sdTwenty= new double[50];
-        // an array to store runtime for n= 20
-        double[] rtTwenty= new double[50]
+        double[] sdTwenty= new double[51];
+        // an array to store runtime for mean, n= 20
+        double[] runtimeMeanTwenty= new double[51];
+        // an array to store runtime for sd, n= 20
+        double[] runtimeSdTwenty= new double[51];
+        // some counter for the arrays
+        int arrIndex= 0;
 
-        System.out.println("Constructing Graph G(n,p), n = " + n);
+        out.println("Constructing Graph G(n,p), n = " + n);
 
         // for increments of p
-        for (double p= 0; p<= 1.00; j+= 0.02) {
+        for (double p= 0; p<= 1.00; p+= 0.02) {
             // Make k instances of the graph
+            int sumCC= 0;
             for (int i= 0; i< k; i++) {
                 RandomGraph rg = new RandomGraph(n, p);
-                int numCC= rg.getCC;
+                int numCC= rg.getCC();
+                // store current k
+                kCC[i]= numCC;
             }
+
+            long startTime= System.currentTimeMillis();
+            // store mean
+            meanTwenty[arrIndex]= compute_mean(kCC, k);
+            long endTime= System.currentTimeMillis();
+            double meanTime= (endTime- startTime)/ (double)(1000);
+            runtimeMeanTwenty[arrIndex]= meanTime;
+
+            // store sd
+            startTime= System.currentTimeMillis();
+            sdTwenty[arrIndex]= std_dev(kCC, k, meanTwenty[arrIndex]);
+            endTime= System.currentTimeMillis();
+            double sdTime= (endTime- startTime)/ (double)(1000);
+            runtimeSdTwenty[arrIndex]= sdTime;
+
+            // inc index
+            arrIndex++;
         }
-        // Print data
+        // Output data 
+        out.println("--- MEAN ---");
+        for(int i= 0; i< meanTwenty.length; i++) {
+            out.println(meanTwenty[i]);
+        }
+        
+        out.println("--- STANDARD DEVIATION ---");
+        for(int i= 0; i< sdTwenty.length; i++) {
+            out.println(sdTwenty[i]);
+        }
+
+        out.println("--- RUNTIME: Mean ---");
+        for(int i= 0; i< runtimeMeanTwenty.length; i++) {
+            out.println(runtimeMeanTwenty[i]);
+        }
+
+        out.println("--- RUNTIME: Standard Deviation ---");
+        for(int i= 0; i< runtimeSdTwenty.length; i++) {
+            out.println(runtimeSdTwenty[i]);
+        }
 
         
-        /***** For n = 100 *****/
+
+        /***** For n = 100 *****
         int n= 100;
         int k= 200;
         // an array to store mean for n= 100
@@ -138,6 +223,7 @@ public class RandomGraph {
             }
         }
         // Print data
+        */
 
 
     }
