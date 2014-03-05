57a465a9fc3e4331ab8a4a0998150028aef43afa
bbaclig <bbaclig@ucsd.edu>
2014-03-05 11:41:30 -0800

    started RandomPointset

---
+++ RandomPointset.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class RandomPointset {
+
+    private class Point {
+        double x;   // x coordinate
+        double y;   // y coordinate
+
+        public Point(double x, double y) {
+            // initialize coordinates
+            this.x= x;
+            this.y= y;
+        }
+    }
+
+    Point[] pointSet;   // list of points in the pointset
+
+    public RandomPointset(int n) {
+        
+    }
+
+/*********** MAIN *********/
+
+    public static void main(String[] args) {
+    
+    }
+
+}
